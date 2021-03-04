31b7c2c52a2b4737b6148a5de7b1587365fb8dab
Geordie King <geordie.king@gmail.com>
2021-03-04 20:12:12 +0000

    prototype gameBoard.js

---
+++ js/tttGrid.js
@@ -0,0 +1,25 @@
+export default class Grid{
+  constructor(){
+    this.g00
+    this.g01
+    this.g02
+    this.g10
+    this.g11
+    this.g12
+    this.g20
+    this.g21
+    this.g22
+    this.gameState
+    this.move
+
+    //then maybe
+    this.row0
+    this.row1
+    this.row2
+    this.col0
+    this.col1
+    this.col2
+    this.dia1
+    this.dia2
+  }
+}
